ec1ef9558f1077bcceabca871c4f7fae24d2cb53
Nicholas Daniel <mackdaniels@protonmail.com>
2018-03-16 02:15:52 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,17 @@
 def unsafe?(speed)
-
+	if speed > 60
+		return true
+	end
+	if speed < 40
+		return true
+	else
+		return false
+	end
 end
 
 
 
 def not_safe?(speed)
-	
+
+	speed < 40? true : speed > 60? true : false
 end
-	
-
-
